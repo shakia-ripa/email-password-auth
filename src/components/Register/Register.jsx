7482bd132bba73288f9345e59b021d9f3ffaef87
shakia-ripa <shakiaripa@gmail.com>
2023-10-07 02:57:35 +0600

    handled error , password hide-show

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,13 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 const Register = () => {
 
     const [registerError, setRegisterError] = useState('');
     const [success, setSuccess] = useState('');
+    const [showPasssword, setShowPassword] = useState(false);
 
     const handleRegister = e => {
         e.preventDefault();
@@ -18,6 +21,10 @@ const Register = () => {
 
         if (password.length < 6) {
             setRegisterError('Password should be at least 6 characters or longer');
+            return;
+        }
+        else if (!/[A-Z]/.test(password)) {
+            setRegisterError('Password should have at least one uppercase character.');
             return;
         }
 
@@ -40,7 +47,17 @@ const Register = () => {
                 <form onSubmit={handleRegister}>
                     <input className="mb-4 border-2 py-2 px-4 rounded-lg w-1/2" type="email" name="email" placeholder="Email Address" id="" required />
                     <br />
-                    <input className="mb-4 border-2 py-2 px-4 rounded-lg w-1/2" type="password" name="password" placeholder="Password" id="" required />
+                    <input
+                        className="mb-4 border-2 py-2 px-4 rounded-lg w-1/2"
+                        type={ showPasssword? "text" : "password"}
+                        name="password"
+                        placeholder="Password"
+                        id="" required />
+                    <span onClick={() => setShowPassword(!showPasssword)}>
+                        {
+                            showPasssword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                        }
+                    </span>
                     <br />
                     <input className="btn btn-accent mb-4 w-1/2" type="submit" value="Register" />
                 </form>
