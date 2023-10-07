a68f71000af1723a03a2b5f15d2222236b971183
shakia-ripa <shakiaripa@gmail.com>
2023-10-07 13:37:34 +0600

    updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,108 @@
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+
 
 
 const Login = () => {
+
+    const [loginError, setLoginError] = useState('');
+    const [success, setSuccess] = useState('');
+    const emailRef = useRef(null);
+
+    const handleLogin = e => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+
+        setLoginError('');
+        setSuccess('');
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+                console.log(result.user);
+                if(result.user.emailVerified){
+                    setSuccess('Login successfull.');
+                }
+                else {
+                    alert('Please verify your email address')
+                }
+            })
+            .catch((error) => {
+                console.log(error.message);
+                setLoginError('Invalid Password');
+            })
+    };
+
+    const handleForgetPassword = () => {
+        const email = emailRef.current.value;
+        if(!email){
+            console.log('Please provide an email', emailRef.current.value);
+            return;
+        }
+        else if(!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email)){
+            console.log('Please write a valid email');
+            return;
+        }
+
+        //send validation email
+        sendPasswordResetEmail(auth, email)
+        .then(() =>{
+            alert('Please check your email');
+        })
+        .catch((error) =>{
+            console.log(error);
+        })
+    }
+
     return (
-        <div>
-            <h2 className="text-3xl">Please Login</h2>
+
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="email"
+                                name="email"
+                                ref={emailRef}
+                                className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password"
+                                name="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a onClick={handleForgetPassword} href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                        {
+                            loginError && <p className="text-xl text-red-500">{loginError}</p>
+                        }
+                        {
+                            success && <p className="text-xl text-green-500 ">{success}</p>
+                        }
+                        <p className="text-lg">New to this website? Please <Link className="text-violet-600 font-semibold" to="/register">Register</Link></p>
+                    </form>
+
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,8 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile } from "firebase/auth";
 import auth from "../../firebase/firebase.config";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Register = () => {
 
@@ -11,9 +12,11 @@ const Register = () => {
 
     const handleRegister = e => {
         e.preventDefault();
+        const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        const accepted = e.target.terms.checked;
+        console.log(name, email, password, accepted);
 
         //reset error
         setRegisterError('');
@@ -27,12 +30,35 @@ const Register = () => {
             setRegisterError('Password should have at least one uppercase character.');
             return;
         }
+        else if(!accepted){
+            setRegisterError('Please accept our terms and condition.');
+            return;
+        }
 
         // create user
         createUserWithEmailAndPassword(auth, email, password)
             .then(result => {
                 console.log(result.user);
-                setSuccess('User created successfully.')
+                setSuccess('User created successfully.');
+
+                // update profile
+                updateProfile(result.user, {
+                    displayName: name,
+                    photoURL: "https://example.com/jane-q-user/profile.jpg"
+                })
+                .then(() =>{
+                    console.log('Profile Updated');
+                })
+                .catch((error) =>{
+                    console.log(error.message);
+                })
+
+                // send verification email
+                sendEmailVerification(result.user)
+                .then(() =>{
+                    alert('Please check your email and verify your account.');
+
+                })
             })
             .catch(error => {
                 console.log(error);
@@ -45,21 +71,31 @@ const Register = () => {
             <div className="mx-auto md:w-1/2">
                 <h2 className="text-4xl my-4">Please Register</h2>
                 <form onSubmit={handleRegister}>
-                    <input className="mb-4 border-2 py-2 px-4 rounded-lg w-1/2" type="email" name="email" placeholder="Email Address" id="" required />
+                    <input className="mb-2 border-2 py-2 px-4 rounded-lg w-1/2" type="text" name="name" placeholder="Your Name" id="" required />
                     <br />
-                    <input
-                        className="mb-4 border-2 py-2 px-4 rounded-lg w-1/2"
-                        type={ showPasssword? "text" : "password"}
-                        name="password"
-                        placeholder="Password"
-                        id="" required />
-                    <span onClick={() => setShowPassword(!showPasssword)}>
-                        {
-                            showPasssword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
-                        }
-                    </span>
+                    <input className="mb-2 border-2 py-2 px-4 rounded-lg w-1/2" type="email" name="email" placeholder="Email Address" id="" required />
                     <br />
-                    <input className="btn btn-accent mb-4 w-1/2" type="submit" value="Register" />
+                    <div className="relative w-1/2">
+                        <input
+                            className="mb-2 border-2 py-2 px-4 rounded-lg w-full"
+                            type={showPasssword ? "text" : "password"}
+                            name="password"
+                            placeholder="Password"
+                            id="" required />
+                        <span className="absolute top-3 right-4" onClick={() => setShowPassword(!showPasssword)}>
+                            {
+                                showPasssword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>
+                            }
+                        </span>
+                    </div>
+                    <br />
+                    <div className="mb-2">
+                        <input type="checkbox" name="terms" id="" />
+                        <label className="ml-3" htmlFor="terms">Accept our <a href="">terms and condition</a>.</label>
+                    </div>
+
+                    <br />
+                    <input className="btn btn-primary mb-4 w-1/2" type="submit" value="Register" />
                 </form>
                 {
                     registerError && <p className="text-red-600 text-xl">
@@ -71,6 +107,9 @@ const Register = () => {
                         {success}
                     </p>
                 }
+                <p>
+                    Already have an account? <Link className="text-violet-600 font-semibold" to="/login">Login</Link>
+                </p>
             </div>
         </div>
     );
